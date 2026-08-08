e2a29dae4098f7019db25d7a099166a9374b999f
Slayde91 <slayde@ceasefire.com.au>
2026-08-09 06:21:18 +1000

    Register quantity labour and productivity APIs

--- src/quantifire/main.py
+++ src/quantifire/main.py
@@ -17,6 +17,8 @@ from . import commercial_models as _commercial_models  # noqa: F401
 from .api import router as api_router
 from .api.guarded_technical import router as guarded_technical_router
 from .api.physical_model import router as physical_model_router
+from .api.productivity import router as productivity_router
+from .api.quantity_labour import router as quantity_labour_router
 from .api.repair_strategy import router as repair_strategy_router
 from .api.workflow import router as workflow_router
 from .api.workflow_actions import router as workflow_actions_router
@@ -106,6 +108,8 @@ def healthz() -> dict[str, str]:
 app.include_router(guarded_technical_router)
 app.include_router(physical_model_router)
 app.include_router(repair_strategy_router)
+app.include_router(quantity_labour_router)
+app.include_router(productivity_router)
 app.include_router(api_router)
 app.include_router(workflow_router)
 app.include_router(workflow_actions_router)
